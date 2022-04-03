2e46728316173f86eba816ac1493be203869c87e
Gasper Spagnolo <gasper.spagnolo@outlook.com>
2022-04-03 23:00:08 +0200

    lolcode-web chall

---
+++ challs/lolcode_web/lolcode-web/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/post', methods=['POST'])
+def process():
+    request_data = request.get_json()
+    lol_code= request_data['lol-code']
+    lol_file = open("h.lol", "w")
+    lol_file.write(lol_code)
+    lol_file.close()
+
+    process = subprocess.Popen(['./lci', 'h.lol'],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if (stdout == b''):
+        return stderr
+    else:
+        return stdout
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8000)
