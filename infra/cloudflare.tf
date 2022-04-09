637acbecfa7f18b70135711574e38d93b3e0f8c3
Andraž Juvan <andraz.juvan@dragonsec.si>
2022-04-09 21:38:20 +0200

    Minor fixes, added connection info

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -5,3 +5,12 @@ data "cloudflare_zone" "dragonsec" {
 data "cloudflare_zone" "dctf" {
   name = "dctf.si"
 }
+
+resource "cloudflare_record" "pwn" {
+  zone_id = data.cloudflare_zone.dctf.id
+  name    = "pwn"
+  proxied = false
+  value   = azurerm_public_ip.challs_pwn.ip_address
+  type    = "A"
+  ttl     = 1
+}
